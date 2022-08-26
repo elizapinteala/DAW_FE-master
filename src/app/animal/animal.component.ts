c92f069ac662e23641062bab4baebed725bdee3a
mihaiioanaalexandra <ioana.mihai.alexandra@gmail.com>
2022-08-26 12:33:39 +0300

    Image

--- src/app/animal/animal.component.ts
+++ src/app/animal/animal.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Animal } from '../Models/animal';
 import { AnimalService } from '../Services/animal.service';
 import { TokenStorageService } from '../_shared/services/token-storage.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-animal',
@@ -11,6 +12,15 @@ import { TokenStorageService } from '../_shared/services/token-storage.service';
 })
 export class AnimalComponent implements OnInit {
 
+    //for image upload
+
+    selectedFile: File;
+    retrievedImage: any = null;
+    base64Data: any;
+    retrieveResonse: any;
+    message: string | undefined;
+    imageName: any;
+    arrayOfBlob = new Array<Blob>();
   
   displayedColumns = ['status', 'name', 'age', 'gender', 'specie', 'breed', 'checkInDate', 'actions'];
   animalList = new MatTableDataSource<Animal>();
@@ -18,9 +28,10 @@ export class AnimalComponent implements OnInit {
   private roles: string[] = [];
   showAdminBoard = false;
   
-  constructor(private animalservice: AnimalService, private tokenStorageService: TokenStorageService) {
+  constructor(private animalservice: AnimalService,
+    private httpClient: HttpClient, private tokenStorageService: TokenStorageService) {
 
-
+      this.selectedFile = new File(this.arrayOfBlob, "Mock.zip", { type: 'application/zip' })
    }
 
 
@@ -34,7 +45,7 @@ export class AnimalComponent implements OnInit {
   }
 
   getAll(){
-    this.animalservice.getAll().subscribe(backendresult=>{
+    this.animalservice.getAll().subscribe((backendresult: Animal[])=>{
       this.animalList.data=backendresult as Animal[];
     })
   }
@@ -43,5 +54,35 @@ export class AnimalComponent implements OnInit {
     console.log(idAnimal)
     this.animalservice.deleteAnimal(idAnimal).subscribe();
     location.reload()
+  }
+
+  //for images
+
+  //Gets called when the user selects an image
+  public onFileChanged(event: { target: { files: (File )[]; }; }) {
+    //Select File
+    this.selectedFile = event.target.files[0];
+  }
+
+
+  //Gets called when the user clicks on submit to upload the image
+  onUpload() {
+   const  httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'image/jpeg' })
+  }
+    //FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
+    const uploadImageData = new FormData();
+    uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+
+    //Make a call to the Spring Boot Application to save the image
+    this.httpClient.post('http://localhost:9191/skimanagement/api/v1/image/upload', uploadImageData, { observe: 'response',  })
+      .subscribe((response: { status: number; }) => {
+        if (response.status === 200) {
+          this.message = 'Image uploaded successfully';
+        } else {
+          this.message = 'Image not uploaded successfully';
+        }
+      }
+      );
   }
 }
